4719a33bc1ab7f03ee79a31c74af1a337aa51f4e
Ahjay803 <93839913+Ahjay803@users.noreply.github.com>
2022-02-10 21:12:13 -0800

    Add files via upload

---
+++ Set.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Corrected by <Allen Atkins>
+namespace Activity_10
+{
+    class Set
+    {
+        private List<int> elements;
+
+        public Set()
+        {
+            elements = new List<int>(8);
+        }
+
+        public bool addElement(int val)
+        {
+        
+            if (containsElement(val))
+            {
+                return false;
+            }
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i])
+                    return true;
+               //else
+                    //return false;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        public Set union(Set rhs)
+        {
+            // created set to hold for Set A
+            Set combined = new Set();
+
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                // changed from rhs to union to combine sets
+                combined.addElement(rhs.elements[i]);
+            }
+
+            // additional for loop to iterate through Set A
+            for(int c = 0; c < this.elements.Count; c++)
+            {
+                combined.addElement(this.elements[c]);
+            }
+
+            return combined;
+        }
+    }
+}
